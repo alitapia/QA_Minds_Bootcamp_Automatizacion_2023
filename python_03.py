cf2decc1604c741c923ff7e044ed0af1d9e594c6
alitapia <alina.tapia@gmail.com>
2023-02-14 19:21:51 -0700

    Practica_03

---
+++ python_03.py
@@ -0,0 +1,10 @@
+nombre = input("Cual es tu nombre?")
+edad = input("Cual es tu edad?")
+ciudad = input("De que ciudad eres?")
+estado_civil = input("Cual es tu estado civil?")
+numero1 = input("Por favor ingresa un número entre 1-100")
+numero2 = input("Por favor ingresa un número entre 100-200")
+
+resultado= int(numero1)/int(numero2)
+
+print(f'Mi nombre es {nombre}, mi edad es {edad}, soy de la ciudad de {ciudad} , mi estado civil es {estado_civil} y mi resultado es {resultado}')
