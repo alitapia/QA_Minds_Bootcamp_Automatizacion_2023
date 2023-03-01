22ab29413d9ce5c1eda2a9433bd7ad686e431b2d
alitapia <alina.tapia@gmail.com>
2023-02-28 18:04:06 -0700

    Practica_08 - Febrero 23 2023

--- python_08_a.py
+++ python_08_a.py
@@ -1,7 +1,22 @@
-tabla = int(input("Ingresa un numero del 1 al 10"))
-MIN = 1
-MAX = 11
-BASE = 1
+#tabla = int(input("Ingresa un numero del 1 al 10"))
+#MIN = 1
+#MAX = 11
+#BASE = 1
 
-for number in range(MIN,MAX,BASE):
-    print(f"{tabla} * {number} = {tabla * number}")
+#for number in range(MIN,MAX,BASE):
+#    print(f"{tabla} * {number} = {tabla * number}")
+
+
+
+
+import random
+MIN = 1
+MAX = 10
+numero = random.randint(MIN, MAX)
+print(numero)
+
+INICIO = 1
+FIN = numero
+
+for number in range(INICIO,FIN):
+    print("*"* number)
