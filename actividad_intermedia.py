8601afd17d7b86fbac6ffc9837f92257b4aad967
alitapia <alina.tapia@gmail.com>
2023-03-08 22:16:24 -0700

    ACTIVIDAD INTERMEDIA - 8 Marzo 2023

--- actividad_intermedia.py
+++ actividad_intermedia.py
@@ -1,4 +1,5 @@
 #libreria titulo, autor, genero
+STATUS = ("NO_INICIADO", "ACTIVO")
 
 
 def add_course(lst: list):
@@ -7,42 +8,37 @@ def add_course(lst: list):
     print("*" * 80)
     curso = input(" Nombre del curso?")
     alumnos = input(" Cantidad de alumnos?")
-    STATUS = ("NO_INICIADO", "ACTIVO")
-
-    status_list = " | ".join(STATUS)
-    estado = input(f"Seleccione un status: {status_list}")
     clases = input("Cantidad de clases")
+    status_list = " | ".join(STATUS)
+    estado_add = input(f"Seleccione un status: {status_list}")
 
 
 
     nueva_entrada = {
         "curso": curso,
         "alumnos": alumnos,
-        "estado": estado,
+        "estado": estado_add,
         "clases": clases
     }
     lst.append(nueva_entrada)
     print(lst)
 
+
 CURSOS = []#Es una lista de diccionarios
+
 def search(lst: list, target: str):
     for index, elem in enumerate(lst):  # accedemos a cada elemento de la lista (en este caso cada elemento es un dictionario)
-        for key, crs in elem.items():  # acedemos a cada llave(k), valor(v) de cada diccionario
-            if crs == target:
-                print(lst[index])
-print("Curso no existe")
+        if lst[index]['curso'] == target:
+            print(lst[index])
+        else:
+            print("Curso no encontrado")
+
 def update(lst:list, target:str , sta:str):
     for index, elem in enumerate(lst):  # accedemos a cada elemento de la lista (en este caso cada elemento es un dictionario)
-        for key, csr in elem.items():  # acedemos a cada llave(k), valor(v) de cada diccionario
-            if csr == target:
-                lst[index]['estado'] = sta
-                print("Estatus del curso actualizado")
-                print(lst[index])
-print("Curso no encontrado")
-
-
-
-
+        if lst[index]['curso'] == target:
+            lst[index]['estado'] = sta
+            print("Estatus del curso actualizado")
+            print(lst[index])
 
 def list(lst:list):
     print(lst)
@@ -52,25 +48,34 @@ while True:
      action = input("Seleccione una acción: SEARCH | ADD | UPDATE | LIST | EXIT ")
      if action == "SEARCH":
          course = input("Que curso buscas?")
+         search(CURSOS,course)
      elif action == "ADD":
          add_course(CURSOS)
      elif action == "UPDATE":
          course = input("Que curso deseas actualizar?")
-         for index, elem in enumerate(CURSOS):  # accedemos a cada elemento de la lista (en este caso cada elemento es un dictionario)
-             for key, crs in elem.items():  # acedemos a cada llave(k), valor(v) de cada diccionario
-                 if crs == course:
-                     print(lst[index])
-                     continue
-             print("Curso no existe")
-         break
          status = ("NO_INICIADO", "ACTIVO")
          status_list = " | ".join(status)
          nuevo_estado = input(f"Seleccione un status: {status_list}")
-         for estado in status:
-             if estado == nuevo_estado:
-                 update(CURSOS,course,nuevo_estado)
-         print("Status no valido")
-         print("Curso no actualizado")
+         for index, elem in enumerate(CURSOS):  # accedemos a cada elemento de la lista (en este caso cada elemento es un dictionario)
+             if CURSOS[index]['curso'] == course:
+                 for st in status:  # accedemos a cada elemento de la lista (en este caso cada elemento es un dictionario)
+                     if nuevo_estado == st:
+                         update(CURSOS, course, nuevo_estado)
+                         break
+                     elif nuevo_estado != st:
+                         print("Curso no actualizado: Status no valido")
+                         print("Intente de nuevo")
+                         break
+                     else:
+                         break
+             elif CURSOS[index]['curso'] != course:
+                 print("Curso no existe en lista")
+                 print("Intente de nuevo")
+                 break
+             else:
+                 print("Intente de nuevo")
+                 break
+
      elif action == "LIST":
          list(CURSOS)
      elif action == "EXIT":
